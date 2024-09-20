2b681b4efa75b50dcc0a551eb0f8876628974c57
GoXomyak <drykilllogic232a@gmail.com>
2024-09-20 18:03:58 +0300

    1

--- magazine.go
+++ magazine.go
@@ -1,12 +1,20 @@
 package Magazine
 
 type Subscriber struct {
-	Name   string
-	Rate   float64
-	Active bool
+	Name        string
+	Rate        float64
+	Active      bool
+	HomeAddress Address
 }
 
 type Employee struct {
-	Name   string
-	Salary float64
+	Name        string
+	Salary      float64
+	HomeAddress Address
+}
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
 }
